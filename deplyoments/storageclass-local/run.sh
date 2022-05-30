4fdfd95fc97d740ef88256bfcc93db27e19cbf67
kammora <kammora@a.a>
2022-05-31 01:45:09 +0200

    up

--- deplyoments/storageclass-local/run.sh
+++ deplyoments/storageclass-local/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 kubectl apply -f ./class.yml
-kubectl patch storageclass <storage-class-name> -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+kubectl patch storageclass local-storage -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
