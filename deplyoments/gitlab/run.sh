195fbe1f1f06402489381227c905614375dd731d
NotNullDev <61083452+NotNullDev@users.noreply.github.com>
2022-05-31 01:29:20 +0200

    up

---
+++ deplyoments/gitlab/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# https://docs.gitlab.com/charts/installation/deployment.html
+# https://docs.gitlab.com/charts/quickstart/index.html
+
+address_public_ip=""
+gitlab_hists_domain="gitlab.dummyhost.com"
+
+
+if [ $address_public_ip = "" ]
+then
+echo "Please edit address_public_ip in script before running this script."
+fi
+
+echo "$address_public_ip           $gitlab_hists_domain" | sudo tee -a /etc/hosts
+
+helm repo add gitlab https://charts.gitlab.io/ \
+&& \
+helm install gitlab gitlab/gitlab \
+    --set global.hosts.domain=DOMAIN \
+    --set certmanager-issuer.email=me@e$gitlab_hists_domain
