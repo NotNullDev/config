2a4d23417a07228b46bebfb914a64db167a26737
kammora <kammora@a.a>
2022-05-30 08:57:04 +0200

    up

--- nfs/server.sh
+++ nfs/server.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
-nfs_folder="/share"
+nfs_folder=""
 
-sudo apt -y install nfs-kernel-server
+if [ $nfs_folder = "" ]; then
+read -r -p "nfs_folder was not set. Use default /share ? [y/N] " response
+case "$response" in
+    [yY][eE][sS]|[yY]) 
+        nfs_folder="/share"
+        break;
+        ;;
+    *)
+        exit
+        ;;
+esac
+fi
+
+
+sudo apt update && sudo apt -y install nfs-kernel-server
 sudo systemctl enable --now nfs-server
 sudo mkdir $nfs_folder
 
