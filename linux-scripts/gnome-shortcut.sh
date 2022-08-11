48f4d0fe71d1ed5ec17780c7a685d05e836511ce
NotNullDev <61083452+NotNullDev@users.noreply.github.com>
2022-08-12 00:36:28 +0200

    up

---
+++ linux-scripts/gnome-shortcut.sh
@@ -0,0 +1,27 @@
+cat<<eof | sudo tee /usr/share/applications/intellij.desktop
+[Desktop Entry]
+Type=Application
+Terminal=false
+Icon[en_US]=/home/$USER/dev-tools/idea/bin/idea.png
+Name[en_US]=IntelliJ
+Exec=/home/$USER/dev-tools/idea/bin/idea
+Name=IntelliJ Idea
+Icon=/home/$USER/dev-tools/idea/bin/idea.png
+eof
+
+sudo chmod 644 /usr/share/applications/intellij.desktop
+sudo chown root:root /usr/share/applications/intellij.desktop
+
+cat<<eof | sudo tee /usr/share/applications/webstorm.desktop
+[Desktop Entry]
+Type=Application
+Terminal=false
+Icon[en_US]=/home/$USER/dev-tools/webstorm/bin/webstorm.png
+Name[en_US]=Webstorm
+Exec=/home/$USER/dev-tools/webstorm/bin/webstorm
+Name=Webstorm
+Icon=/home/$USER/dev-tools/webstorm/bin/webstorm.png
+eof
+
+sudo chmod 644 /usr/share/applications/webstorm.desktop
+sudo chown root:root /usr/share/applications/webstorm.desktop
