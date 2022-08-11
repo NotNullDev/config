b94c9df819f10f8048ba31c96f98847d99517eec
NotNullDev <61083452+NotNullDev@users.noreply.github.com>
2022-08-11 23:57:44 +0200

    up

--- linux-scripts/zsh-install.sh
+++ linux-scripts/zsh-install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-sudo apt install curl zsh -y
+sudo apt install curl zsh wget -y
 sudo chsh $USER -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 echo "soruce ~/.zshrc"
+
+# mkdir -p $ZSH_CUSTOM/themes
+# wget https://github.com/ChesterYue/ohmyzsh-theme-passion/blob/11b1a03575ac826646ec438f6390a077a0bd85d4/passion.zsh-theme
+# mv passion.zsh-theme $ZSH_CUSTOM/themes
+# echo ZSH_THEME="passion" >> ~/.zshrc
+# echo source ~/.zshrc
