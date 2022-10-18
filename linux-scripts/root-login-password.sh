b77bedc6ae0b8ed50441346b76ebb68faf51ddf6
NotNullDev <notnulldev0@gmail.com>
2022-10-18 15:09:20 +0200

    up

--- linux-scripts/root-login-password.sh
+++ linux-scripts/root-login-password.sh
@@ -1,3 +1,1 @@
-yes qwqw | sudo passwd root;
-sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+yes qwqw | sudo passwd root && sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config && sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config && sudo systemctl restart sshd
